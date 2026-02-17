4454f3081fb0ce4e5020e906913a2236272471da
Avinashpalofficial <apal73451@gmail.com>
2026-02-17 05:31:47 +0530

    feat(update DB connection):add adapter

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,10 @@
-import {PrismaClient} from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as {
-     prisma : PrismaClient | undefined
-}
-export const prisma = globalForPrisma.prisma ??
-                      new  PrismaClient({
-                        datasourceUrl :process.env.DATABASE_URL
-                      })
-if(process.env.NODE_ENV !=="production"){
-    globalForPrisma.prisma = prisma
-}                   
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
