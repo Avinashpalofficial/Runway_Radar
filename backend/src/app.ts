21172748d0957c5464783fb685ecb16eea27f1e3
Avinashpalofficial <apal73451@gmail.com>
2026-02-18 16:33:59 +0530

    add  a financialRouter in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import router from "./routes/authRoutes";
-
+import financialRouter from "./routes/financialProfile.routes";
+import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser());
 app.use("/api/v1/auth", router);
+app.use("/api/v1", financialRouter);
 
 export default app;
