13ddf46cb3d37f6b73897bcbf43dfd5ba8af66d6
Avinashpalofficial <apal73451@gmail.com>
2026-02-21 20:28:19 +0530

    add errorMiddleware in app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,12 @@ import express from "express";
 import router from "./routes/authRoutes";
 import financialRouter from "./routes/financialProfile.routes";
 import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/errorMiddleware";
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", router);
 app.use("/api/v1", financialRouter);
-
+app.use(errorMiddleware);
 export default app;
