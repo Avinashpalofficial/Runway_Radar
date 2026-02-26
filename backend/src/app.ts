7dfc67b17c199dc75fd274fd17f87004bd5bb77e
Avinashpalofficial <apal73451@gmail.com>
2026-02-26 10:26:37 +0530

    update the app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,13 @@ import router from "./routes/authRoutes";
 import financialRouter from "./routes/financialProfile.routes";
 import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
+import subscriptionRouter from "./routes/subscription.routes";
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", router);
-app.use("/api/v1", financialRouter);
+app.use("/api/v1/financial", financialRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
 app.use(errorMiddleware);
 export default app;
