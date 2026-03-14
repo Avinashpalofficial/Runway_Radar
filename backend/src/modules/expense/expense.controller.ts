bcff2f8330fcc3c1cb4cbe50647032c2450dc165
Avinashpalofficial <apal73451@gmail.com>
2026-03-14 10:46:18 +0530

    create a get Expense Controller

---
+++ backend/src/modules/expense/expense.controller.ts
@@ -0,0 +1,45 @@
+import { Prisma } from "@prisma/client";
+import * as expenseService from "../expense/expense.service";
+import { catchAsyncError } from "../../middlewares/catchAsync";
+import { errorMiddleware } from "../../middlewares/errorMiddleware";
+import { ErrorHandler } from "../../utils/ErrorHandler";
+
+export const create = catchAsyncError(async (req, res, next) => {
+  const userId = req.user.id;
+  console.log("userId:", userId);
+
+  const { title, currency, amount, category } = req.body;
+  const expense = await expenseService.createExpense(
+    userId,
+    title,
+    currency,
+    amount,
+    category,
+  );
+
+  res.status(200).json({
+    success: true,
+    expense,
+  });
+});
+
+/**
+ * Get Expense Controller
+ */
+
+export const getAll = catchAsyncError(async (req, res, next) => {
+  const userId = req.user.id;
+  const expense = await expenseService.getExpense(userId);
+  if (!expense) {
+    throw new ErrorHandler("user expense is not exist", 401);
+  }
+  res.status(200).json({
+    success: true,
+    expense,
+  });
+});
+
+/**
+ *
+ * Update Expense Controller
+ */
