d4c1942ba027c66eb85844e082390f2165422716
Avinashpalofficial <apal73451@gmail.com>
2026-03-20 13:12:56 +0530

    add a login api

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -5,3 +5,8 @@ export const SingupUser = async (data: SingupData): Promise<User> => {
   const res = await api.post<User>("/auth/user/register", data);
   return res.data;
 };
+
+export const LoginUser = async (data: LoginData): Promise<User> => {
+  const res = await api.post<User>("/auth/user/login", data);
+  return res.data;
+};
