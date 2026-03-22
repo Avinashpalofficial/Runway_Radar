88da84c14e7638fbbbf879f1d293ef9ca927d51c
Avinashpalofficial <apal73451@gmail.com>
2026-03-22 14:08:27 +0530

    add a auth store so that we can manage the auth globally

---
+++ frontend/src/store/auth.store.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+
+import { User } from "../types/auth.types";
+
+interface AuthState {
+  user: User | null;
+  isAuthenticated: boolean;
+
+  setUser: (user: User | null) => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  isAuthenticated: false,
+  setUser: (user) => {
+    set({
+      user,
+      isAuthenticated: !!user,
+    });
+  },
+}));
