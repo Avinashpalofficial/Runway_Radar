fb571f4a2198179fbd30a5c771d4979fddd9ab94
Avinashpalofficial <apal73451@gmail.com>
2026-03-22 13:52:15 +0530

    add the financeProfile api

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -1,12 +1,22 @@
-import { SingupData, LoginData, User } from "../types/auth.types";
+import {
+  SignupData,
+  LoginData,
+  User,
+  getCurrentUserResponse,
+  AuthResponse,
+} from "../types/auth.types";
 import { api } from "../lib/api";
 
-export const SingupUser = async (data: SingupData): Promise<User> => {
+export const SingupUser = async (data: SignupData): Promise<User> => {
   const res = await api.post<User>("/auth/user/register", data);
   return res.data;
 };
 
 export const LoginUser = async (data: LoginData): Promise<User> => {
-  const res = await api.post<User>("/auth/user/login", data);
-  return res.data;
+  const res = await api.post<AuthResponse>("/auth/user/login", data);
+  return res.data.user;
+};
+export const getCurrentUser = async (): Promise<User> => {
+  const res = await api.get<getCurrentUserResponse>("auth/user/me");
+  return res.data.user;
 };
