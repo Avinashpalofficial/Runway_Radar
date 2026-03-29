7cfe59eae501a5ddae151f3fd914b21a5542e041
Avinashpalofficial <apal73451@gmail.com>
2026-03-29 14:01:44 +0530

    fix the landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -96,13 +96,13 @@ const Nav = () => (
           Features
           <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-brand transition-all group-hover:w-full" />
         </Link>
-        <Link
+        {/* <Link
           to="/pricing"
           className="hover:text-white transition-colors relative group"
         >
           Pricing
           <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-brand transition-all group-hover:w-full" />
-        </Link>
+        </Link> */}
         <Link
           to="/docs"
           className="hover:text-white transition-colors relative group"
