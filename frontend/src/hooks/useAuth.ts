97825f6f59c5dbc7a2fc7557f004c5cb20ca300b
Avinashpalofficial <apal73451@gmail.com>
2026-04-04 19:00:12 +0530

    update the hooks

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -3,20 +3,27 @@ import {
   UseMutationResult,
   useQuery,
 } from "@tanstack/react-query";
-import { LoginData, SignupData, User } from "../types/auth.types";
+import { LoginData, LogoutData, SignupData, User } from "../types/auth.types";
 import { useAuthStore } from "../store/auth.store";
-import { LoginUser, SingupUser, getCurrentUser } from "../api/auth.api";
+import {
+  LoginUser,
+  SingupUser,
+  logoutUser,
+  getCurrentUser,
+} from "../api/auth.api";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 interface UseAuthReturn {
   login: (data: LoginData) => void;
   signup: (data: SignupData) => void;
+  logout: () => void;
   isLoading: boolean;
   error: unknown;
   user: User | null;
 }
 
 export const useAuth = (): UseAuthReturn => {
+  const clearUser = useAuthStore((state) => state.clearUser);
   const navigate = useNavigate();
   const setUser = useAuthStore((state) => state.setUser);
   const user = useAuthStore((state) => state.user);
@@ -34,6 +41,14 @@ export const useAuth = (): UseAuthReturn => {
       mutationFn: SingupUser,
       onSuccess: handleAuthSuccess,
     });
+  const logoutMutation: UseMutationResult<LogoutData, unknown, void> =
+    useMutation({
+      mutationFn: logoutUser,
+      onSuccess: () => {
+        clearUser(); // store se user clear karo
+        navigate("/login");
+      },
+    });
   const { data, isLoading: isUserLoading } = useQuery({
     queryKey: ["currentUser"],
     queryFn: getCurrentUser,
@@ -49,9 +64,13 @@ export const useAuth = (): UseAuthReturn => {
   return {
     login: loginMutation.mutate,
     signup: signupMutation.mutate,
+    logout: logoutMutation.mutate,
     isLoading:
-      loginMutation.isPending || signupMutation.isPending || isUserLoading,
-    error: loginMutation.error || signupMutation.error,
+      loginMutation.isPending ||
+      signupMutation.isPending ||
+      logoutMutation.isPending ||
+      isUserLoading,
+    error: loginMutation.error || signupMutation.error || logoutMutation.error,
     user,
   };
 };
