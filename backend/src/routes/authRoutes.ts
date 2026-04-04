5886c3931f7658464c323bc0efd70f18b6245e0c
Avinashpalofficial <apal73451@gmail.com>
2026-04-04 19:33:31 +0530

    fix the api

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -14,6 +14,6 @@ const router = Router();
 
 router.post("/user/register", validate(registerSchema), registerUser);
 router.post("/user/login", validate(loginSchema), loginUser);
-router.post("user/logout", authUser, logoutUser);
+router.post("/user/logout", authUser, logoutUser);
 router.get("/user/me", authUser, getCurrentUser);
 export default router;
