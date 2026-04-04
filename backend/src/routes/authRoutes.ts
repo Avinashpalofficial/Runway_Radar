90a75d52f2aa50f0a5fb6864bde9ae9eea985198
Avinashpalofficial <apal73451@gmail.com>
2026-04-04 18:32:23 +0530

    create a logout api

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -4,6 +4,7 @@ import { registerSchema } from "../validate/registerValidation";
 import {
   getCurrentUser,
   loginUser,
+  logoutUser,
   registerUser,
 } from "../modules/auth/authController";
 import { loginSchema } from "../validate/loginValidation";
@@ -13,5 +14,6 @@ const router = Router();
 
 router.post("/user/register", validate(registerSchema), registerUser);
 router.post("/user/login", validate(loginSchema), loginUser);
+router.post("user/logout", authUser, logoutUser);
 router.get("/user/me", authUser, getCurrentUser);
 export default router;
